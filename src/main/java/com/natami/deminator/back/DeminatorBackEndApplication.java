43dba5f0e9e2a9902bbe5a3d4fd3100119a066d2
Nathaël Noguès <nathael.nogues@gmail.com>
2021-06-10 20:01:58 +0200

    Initial commit

---
+++ src/main/java/com/natami/deminator/back/DeminatorBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.natami.deminator.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeminatorBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeminatorBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natami/deminator/back/DeminatorBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natami.deminator.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeminatorBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
